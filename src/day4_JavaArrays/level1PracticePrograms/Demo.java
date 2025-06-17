b210e9d3b244065df0c669b7975767ed3b0b98de
arpit1674 <arpitdhakad8057@gmail.com>
2025-06-17 12:54:21 +0530

    [Arpit Dhakar] add .Q2_NumberSignAndCompare

---
+++ src/day4_JavaArrays/level1PracticePrograms/Demo.java
@@ -0,0 +1,16 @@
+package day4_JavaArrays.level1PracticePrograms;
+public class Demo{
+	static int x=10;
+	static int y=15;
+	public static void m1() {
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
+	class Solution{
+	public static void main(String...args) {
+		Demo demo = new Demo();
+		demo.m1();
+		
+	}
+}
---
+++ src/day4_JavaArrays/level1PracticePrograms/Q2_NumberSignAndCompare.java
@@ -0,0 +1,47 @@
+package day4_JavaArrays.level1PracticePrograms;
+
+import java.util.Scanner;
+
+public class Q2_NumberSignAndCompare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+
+        // Input
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+        }
+
+        // Process each number
+        for (int i = 0; i < numbers.length; i++) {
+            int num = numbers[i];
+
+            if (num > 0) {
+                if (num % 2 == 0) {
+                    System.out.println("Number " + num + " is positive and even.");
+                } else {
+                    System.out.println("Number " + num + " is positive and odd.");
+                }
+            } else if (num < 0) {
+                System.out.println("Number " + num + " is negative.");
+            } else {
+                System.out.println("Number " + num + " is zero.");
+            }
+        }
+
+        // Compare first and last element
+        int first = numbers[0];
+        int last = numbers[numbers.length - 1];
+
+        if (first == last) {
+            System.out.println("First and last elements are equal.");
+        } else if (first > last) {
+            System.out.println("First element is greater than the last element.");
+        } else {
+            System.out.println("First element is less than the last element.");
+        }
+
+        sc.close();
+    }
+}
