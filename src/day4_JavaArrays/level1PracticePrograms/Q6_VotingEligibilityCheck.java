368ff7cef43dac60f803a197c4001b21c6d2082f
arpit1674 <arpitdhakad8057@gmail.com>
2025-06-17 10:58:15 +0530

    [arpit dhakar] add .VotingEligibilityCheck

---
+++ src/day4_JavaArrays/level1PracticePrograms/Q6_VotingEligibilityCheck.java
@@ -0,0 +1,28 @@
+package day4_JavaArrays.level1PracticePrograms;
+
+import java.util.Scanner;
+
+public class Q6_VotingEligibilityCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ages = new int[10];
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
+        }
+
+        System.out.println("\nVoting Eligibility Results:");
+        for (int i = 0; i < ages.length; i++) {
+            if (ages[i] < 0) {
+                System.out.println("Student " + (i + 1) + ": Invalid age.");
+            } else if (ages[i] >= 18) {
+                System.out.println("Student " + (i + 1) + " with age " + ages[i] + " can vote.");
+            } else {
+                System.out.println("Student " + (i + 1) + " with age " + ages[i] + " cannot vote.");
+            }
+        }
+
+        sc.close();
+    }
+}
