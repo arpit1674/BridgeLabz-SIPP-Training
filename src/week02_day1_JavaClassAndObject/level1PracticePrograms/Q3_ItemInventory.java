c3444adae9322a8c18eaa87dc8aa7fc46fef9dba
arpit1674 <arpitdhakad8057@gmail.com>
2025-06-23 22:44:43 +0530

    [Arpit Dhakar] commit add .ItemInventory

--- src/week02_day1_JavaClassAndObject/level1PracticePrograms/Q3_ItemInventory.java
+++ src/week02_day1_JavaClassAndObject/level1PracticePrograms/Q3_ItemInventory.java
@@ -8,27 +8,27 @@ public class Q3_ItemInventory {
     double price;
 
     void displayDetails(int quantity) {
-        System.out.println("Item Code: " + itemCode);
-        System.out.println("Item Name: " + itemName);
-        System.out.println("Price per Item: ₹" + price);
-        System.out.println("Total Cost for " + quantity + " units: ₹" + (price * quantity));
+        System.out.println("Item Code : " + itemCode);
+        System.out.println("Item Name : " + itemName);
+        System.out.println("Price per Item : ₹" + price);
+        System.out.println("Total Cost for " + quantity + " units : ₹" + (price * quantity));
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Q3_ItemInventory item = new Q3_ItemInventory();
 
-        System.out.print("Enter Item Code: ");
+        System.out.print("Enter Item Code : ");
         item.itemCode = sc.nextInt();
         sc.nextLine(); // consume newline
 
-        System.out.print("Enter Item Name: ");
+        System.out.print("Enter Item Name : ");
         item.itemName = sc.nextLine();
 
-        System.out.print("Enter Price: ");
+        System.out.print("Enter Price : ");
         item.price = sc.nextDouble();
 
-        System.out.print("Enter Quantity: ");
+        System.out.print("Enter Quantity : ");
         int quantity = sc.nextInt();
 
         item.displayDetails(quantity);
