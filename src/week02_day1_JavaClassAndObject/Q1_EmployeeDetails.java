123d2a61909ac6d4784b87b47c9313b82ee3d359
arpit1674 <arpitdhakad8057@gmail.com>
2025-06-23 22:36:44 +0530

    [Arpit Dhakar] commit add .EmployeeDetails

--- src/week02_day1_JavaClassAndObject/Q1_EmployeeDetails.java
+++ src/week02_day1_JavaClassAndObject/Q1_EmployeeDetails.java
@@ -1,30 +1,32 @@
 package week02_day1_JavaClassAndObject;
-import java.util.*;
+
+import java.util.Scanner;
 
 public class Q1_EmployeeDetails {
     String name;
     int id;
     double salary;
 
-    public void getDetails() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Employee Name: ");
-        name = sc.nextLine();
-        System.out.print("Enter Employee ID: ");
-        id = sc.nextInt();
-        System.out.print("Enter Salary: ");
-        salary = sc.nextDouble();
-        sc.close();    }
-
-    public void displayDetails() {
+    void displayDetails() {
         System.out.println("Employee Name: " + name);
         System.out.println("Employee ID: " + id);
-        System.out.println("Salary: " + salary);
+        System.out.println("Salary: ₹" + salary);
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Q1_EmployeeDetails emp = new Q1_EmployeeDetails();
-        emp.getDetails();
+
+        System.out.print("Enter Employee Name: ");
+        emp.name = sc.nextLine();
+
+        System.out.print("Enter Employee ID: ");
+        emp.id = sc.nextInt();
+
+        System.out.print("Enter Salary: ");
+        emp.salary = sc.nextDouble();
+
         emp.displayDetails();
+        sc.close();
     }
 }
