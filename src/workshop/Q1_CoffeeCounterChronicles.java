4f1d0050344a2bbb26fd25ffa3c43132558066af
arpit1674 <arpitdhakad8057@gmail.com>
2025-06-28 10:13:43 +0530

    [Arpit Dhakar] commit add .Q1_CoffeeCounterChronicles

---
+++ src/workshop/Q1_CoffeeCounterChronicles.java
@@ -0,0 +1,36 @@
+package workshop;
+import java.util.Scanner;
+
+public class Q1_CoffeeCounterChronicles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String choice;
+        while (true) {
+            System.out.println("Enter coffee type (Espresso / Latte / Cappuccino) or 'exit' to quit:");
+            choice = sc.next();
+            if (choice.equalsIgnoreCase("exit")) break;
+            System.out.println("Enter quantity:");
+            int quantity = sc.nextInt();
+            double price = 0;
+            switch (choice.toLowerCase()) {
+                case "espresso":
+                    price = 100;
+                    break;
+                case "latte":
+                    price = 120;
+                    break;
+                case "cappuccino":
+                    price = 140;
+                    break;
+                default:
+                    System.out.println("Invalid coffee type");
+                    continue;
+            }
+            double total = price * quantity;
+            double gst = total * 0.18;
+            double bill = total + gst;
+            System.out.println("Bill with GST: ₹" + bill);
+        }
+        sc.close();
+    }
+}
