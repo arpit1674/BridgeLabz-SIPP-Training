ae8deb3dd55118dca0fa6ebbdb972309cc7226eb
arpit1674 <arpitdhakad8057@gmail.com>
2025-07-09 21:21:57 +0530

    [Arpit Dhakar] commit add .HeapSort.java

---
+++ src/week03_day3_SortingAlgorithms/HeapSort.java
@@ -0,0 +1,28 @@
+package week03_day3_SortingAlgorithms;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] salary = {35000, 25000, 40000, 30000, 28000};
+        heapSort(salary);
+        for (int s : salary) System.out.print(s + " ");
+    }
+
+    static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0]; arr[0] = arr[i]; arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i, left = 2 * i + 1, right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+        if (largest != i) {
+            int swap = arr[i]; arr[i] = arr[largest]; arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
+    }
+}
