80d898df921fea6704be5a218d62e05934824042
arpit1674 <arpitdhakad8057@gmail.com>
2025-07-14 10:29:49 +0530

    [Arpit Dhakar] commit add .ReverseString.java

---
+++ src/week03_day4and5_LinearAndBinarySearch/StringBuilder/ReverseString.java
@@ -0,0 +1,9 @@
+package week03_day4and5_LinearAndBinarySearch.StringBuilder;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println("Reversed String: " + sb.reverse());
+    }
+}
