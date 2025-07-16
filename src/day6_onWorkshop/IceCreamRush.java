463dc1dfef960f5341f42b4de6c92e1a4eaf31a8
arpit1674 <arpitdhakad8057@gmail.com>
2025-07-16 10:16:29 +0530

    [Arpit Dhakar] commit add.IceCreamRush.java

--- src/day6_onWorkshop/IceCreamRush.java
+++ src/day6_onWorkshop/IceCreamRush.java
@@ -1,6 +1,6 @@
 package day6_onWorkshop;
 import java.util.Scanner;
-public class Main {
+public class IceCreamRush {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int count = 1;
