e03c09fdc4a7971ec60084325ed6e59dcdefdd4c
arpit1674 <arpitdhakad8057@gmail.com>
2025-07-16 13:31:52 +0530

    [Arpit Dhakar] commit add .IceCreamRush.java

--- src/day6_onWorkshop/IceCreamRush.java
+++ src/day6_onWorkshop/IceCreamRush.java
@@ -3,35 +3,39 @@ import java.util.Scanner;
 public class IceCreamRush {
 	Scanner sc = new Scanner(System.in);	
 	void takeInputs(int[] arr) {
-		int count = 1;
-		for(int i=0; i<arr.length-1; i++) {
-			System.out.print("Enter sale of flavor" + count+" (between 1 to 1000) :");
+		int count = 0;
+		for (int i = 0; i < arr.length - 1; i++) {
 			count++;
-			arr[i] = sc.nextInt();
-			while(true) {
-				if(arr[i] < 0 || arr[i] > 1000) {
-					System.out.println("Invalid Number! Type (between 1 to 1000) for flavor" + --count + " :");
+			while (true) {
+				System.out.print("Enter sale of flavor" + count + " (between 1 to 1000) : ");
+				try {
 					arr[i] = sc.nextInt();
+					if (arr[i] < 1 || arr[i] > 1000) {
+						System.out.println("Invalid Number! Please enter a between 1 to 1000.");
+						continue;
+					}
+					break;
+				} catch (Exception e) {
+					System.out.println("Invalid Input! Please enter a number.");
+					sc.nextLine(); 
 				}
-				break;
 			}
 		}
 		System.out.println();
 	}
+
 	
 	void iceCreamSalesSorter(int[] arr) {
-		int temp = 0;
-		
 		for(int i=0; i< arr.length-1; i++) {
 			for(int j=0; j< arr.length- i- 1; j++ ) {
 				if(arr[j]> arr[j+1]) {
-					temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+					arr[j] = arr[j] ^ arr[j+1];
+					arr[j+1] = arr[j] ^ arr[j+1];
+					arr[j] = arr[j] ^ arr[j+1];
 				}	
 			}
 		}
-		System.out.println("After Bubble Sort: ");
+		System.out.println("Resultant Array after Bubble Sort: ");
 		for(int index : arr) System.out.print(index + " ");
 	}
 	
--- src/day6_onWorkshop/Main.java
+++ src/day6_onWorkshop/Main.java
@@ -4,6 +4,7 @@ public class Main {
 		IceCreamRush obj = new IceCreamRush();
 		int[] arr = new int[9];
 		obj.takeInputs(arr);
+		System.out.println("Resultant array:");
 		obj.printArray(arr);
 		obj.iceCreamSalesSorter(arr);
 	}
