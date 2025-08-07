ed9f6a5f2b385a34bb499df4485a4cc1d8f26ac3
arpit1674 <arpitdhakad8057@gmail.com>
2025-08-07 10:34:47 +0530

    [Arpit Dhakar] commit add .VotingSystem.java

--- src/week04_day2_SubmissionOfJavaCollections/VotingSystem.java
+++ src/week04_day2_SubmissionOfJavaCollections/VotingSystem.java
@@ -1,5 +1,20 @@
 package week04_day2_SubmissionOfJavaCollections;
+import java.util.*;
 
 public class VotingSystem {
+    public static void main(String[] args) {
+        Map<String, Integer> voteMap = new HashMap<>();
+        voteMap.put("Candidate A", 0);
+        voteMap.put("Candidate B", 0);
 
+        String[] votes = {"Candidate A", "Candidate A", "Candidate B", "Candidate A"};
+
+        for (String vote : votes) {
+            voteMap.put(vote, voteMap.getOrDefault(vote, 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : voteMap.entrySet()) {
+            System.out.println(entry.getKey() + " received " + entry.getValue() + " votes.");
+        }
+    }
 }
