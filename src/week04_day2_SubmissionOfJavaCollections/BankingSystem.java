30a2feda07b83de8d9d35507f73e76921195231c
arpit1674 <arpitdhakad8057@gmail.com>
2025-08-07 10:36:32 +0530

    [Arpit Dhakar] commit add .BankingSystem.java

--- src/week04_day2_SubmissionOfJavaCollections/BankingSystem.java
+++ src/week04_day2_SubmissionOfJavaCollections/BankingSystem.java
@@ -1,5 +1,15 @@
 package week04_day2_SubmissionOfJavaCollections;
 
-public class BankingSystem {
+import java.util.*;
 
+public class BankingSystem {
+    public static void main(String[] args) {
+        Map<Integer, Double> accountBalance = new HashMap<>();
+        accountBalance.put(1001, 25000.0);
+        accountBalance.put(1002, 18000.0);
+
+        for (Map.Entry<Integer, Double> entry : accountBalance.entrySet()) {
+            System.out.println("Account: " + entry.getKey() + ", Balance: ₹" + entry.getValue());
+        }
+    }
 }
