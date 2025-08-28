06815a062cd56461925aa1fecb61df8564000ee9
arpit1674 <arpitdhakad8057@gmail.com>
2025-08-28 11:31:37 +0530

    [Arpit Dhakar] add .StringUtilsTest.java

---
+++ src/week04_day5_SubmissionOfJUnit/StringUtils.java
@@ -0,0 +1,15 @@
+package week04_day5_SubmissionOfJUnit;
+
+public class StringUtils {
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        return str.equalsIgnoreCase(reverse(str));
+    }
+
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
