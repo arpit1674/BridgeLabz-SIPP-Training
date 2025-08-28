24ba387b2666f1078e46a1a461a6979d84efa1f5
arpit1674 <arpitdhakad8057@gmail.com>
2025-08-28 11:49:47 +0530

    [Arpit Dhakar] add.Filerocessor.java

---
+++ src/week04_day5_SubmissionOfJUnit/FileProcessorTest.java
@@ -0,0 +1,25 @@
+package week04_day5_SubmissionOfJUnit;
+
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileProcessorTest {
+
+    FileProcessor processor = new FileProcessor();
+    String filename = "testFile.txt";
+
+    @Test
+    void testFileWriteAndRead() throws IOException {
+        processor.writeToFile(filename, "Hello World");
+        String content = processor.readFromFile(filename);
+        assertEquals("Hello World", content);
+        assertTrue(new File(filename).exists());
+    }
+
+    @Test
+    void testReadNonExistentFile() {
+        assertThrows(IOException.class, () -> processor.readFromFile("nonexistent.txt"));
+    }
+}
